af5aa8f95edcefe9fbcf0eaed3183ec3b6758f9f
Francesco Fonte <webffranz@gmail.com>
2011-07-22 01:12:47 +0200

    add-to-cart

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-/*  Your JavaScript  */
+/*  Your JavaScript for Resource  */
+
+$(document).ready(function(){
+
+	Utils.init();
+
+});
+
+var Utils  = {
+	
+	init : function(){
+	
+		$('.menutop').append(' &mdash; <a href="http://www.premiumpixels.com/freebies/add-to-cart-buttons-psd/" target="_blank">Inspired by: Pixel Premium</a> ')
+	
+	}
+	
+}
